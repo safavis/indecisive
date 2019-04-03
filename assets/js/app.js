ed0b2f26a92ccb6362c2fdad3bc508d3808c7513
C.Nguyen <cdkedits@gmail.com>
2019-04-02 18:54:17 -0700

    added yelp api key

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyALJmJ2VVKSDLb7wNk3HGxdXM1OH_KfE30",
+  authDomain: "indecisive-a9a51.firebaseapp.com",
+  databaseURL: "https://indecisive-a9a51.firebaseio.com",
+  projectId: "indecisive-a9a51",
+  storageBucket: "indecisive-a9a51.appspot.com",
+  messagingSenderId: "848170258525"
+}
+
+firebase.initializeApp(config)
+
+const APIkey = {
+  method: "GET",
+  mode: "cors",
+  cache: "no-cache",
+  credentials: "same-origin",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer{M7IgpDGg-9kXeDXpl5Yj9A9_33reRjvfXANnx3RvJzphIo_pAXUEvP5FubmdzBV32ehGAyXtlzo0_hjlxzrBPi4O705EnwbTMXBu1v3rbP78tARuwNYSElA1WBqjXHYx}`
+  },
+  referrer: "no-referrer", // no-referrer, *client
+  body: JSON.stringify(data), // body data type must match "Content-Type" header
+}
+
+fetch(url, options)
+  .then(r => r.json())
+  .then(r => {
+    console.log('here is the response: ', r)
+  })
