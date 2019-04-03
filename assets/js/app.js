ba11d4972f3591f8114da9723c50f28288d680f3
C.Nguyen <cdkedits@gmail.com>
2019-04-02 21:15:59 -0700

    working yelp api

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,24 +7,77 @@ const config = {
   storageBucket: "indecisive-a9a51.appspot.com",
   messagingSenderId: "848170258525"
 }
-
 firebase.initializeApp(config)
+let db = firebase.firestore()
+// 
 
-const apiKey = {
+// Yelp API code
+let URL = 'https://api.yelp.com/v3/businesses/search?location=40515&term&categories=vet&limit=10';
+
+const API_KEY = 'M7IgpDGg-9kXeDXpl5Yj9A9_33reRjvfXANnx3RvJzphIo_pAXUEvP5FubmdzBV32ehGAyXtlzo0_hjlxzrBPi4O705EnwbTMXBu1v3rbP78tARuwNYSElA1WBqjXHYx';
+
+let queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
+
+fetch(queryURL, {
+
   method: "GET",
-  mode: "cors",
-  cache: "no-cache",
-  credentials: "same-origin",
   headers: {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer{M7IgpDGg-9kXeDXpl5Yj9A9_33reRjvfXANnx3RvJzphIo_pAXUEvP5FubmdzBV32ehGAyXtlzo0_hjlxzrBPi4O705EnwbTMXBu1v3rbP78tARuwNYSElA1WBqjXHYx}`
-  },
-  referrer: "no-referrer", // no-referrer, *client
-  body: JSON.stringify(data), // body data type must match "Content-Type" header
+    "accept": "application/json",
+    "x-requested-with": "xmlhttprequest",
+    "Access-Control-Allow-Origin": "*",
+    "Authorization": `Bearer ${API_KEY}`
+  }
+}).then(r => r.json())
+
+  .then(data => {
+    console.log(data)
+  });
+// 
+
+// Contact Form Firebase Submissions:
+  // Array of buttons that close messages
+const deleteBtns = document.querySelectorAll('.delete');
+
+  // Submit form to Firebase
+document.querySelector(`.contactsbmt`).addEventListener(`click`, e => {
+  e.preventDefault()
+  let x = document.querySelector(`.option`).selectedIndex
+  let id = db.collection(`submissions`).doc().id
+  let name = document.querySelector(`.name`).value
+  let email = document.querySelector(`.email`).value
+  let subject = document.getElementsByTagName(`option`)[x].value
+  let message = document.querySelector(`.userMessage`).value
+  if (name === `` || email === `` || message === ``) {
+    document.querySelector(`.is-danger`).style.display = `block`
+  } else {
+    db.collection(`submissions`).doc(id).set({
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    })
+    document.querySelector(`.name`).value = ``
+    document.querySelector(`.email`).value = ``
+    document.querySelector(`.userMessage`).value = ``
+    document.querySelector(`.is-success`).style.display = `block`
+    console.log(name)
+    console.log(email)
+    console.log(message)
+  }
+})
+// 
+
+// Hiding messages
+const hideElem = (elem) => {
+  elem.style.display = `none`
 }
 
-fetch(url, apiKey)
-  .then(r => r.json())
-  .then(r => {
-    console.log('here is the response: ', r)
+for (let i = 0; i < deleteBtns.length; i++) {
+  // console.log("Delete Item", deleteBtns[i])
+  deleteBtns[i].addEventListener(`click`, e => {
+    hideElem()
   })
+}
+// 
+
+
