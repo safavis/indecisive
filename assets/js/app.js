30b666f91742285313c73e2fafe9f9de92397bcd
C.Nguyen <cdkedits@gmail.com>
2019-04-06 10:59:40 -0700

    new nested fetch request line 31

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,75 @@
 // Yelp API code
-// let URL = 'https://api.yelp.com/v3/businesses/search?location=40515&term&categories=vet&limit=10';
-// let URL = `https://api.yelp.com/v3/businesses/search?location=${searchInput}&limit=10`;
+let URL
 
 const API_KEY = 'M7IgpDGg-9kXeDXpl5Yj9A9_33reRjvfXANnx3RvJzphIo_pAXUEvP5FubmdzBV32ehGAyXtlzo0_hjlxzrBPi4O705EnwbTMXBu1v3rbP78tARuwNYSElA1WBqjXHYx';
 
 let queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
 
+let yelpObject = {
+  method: "GET",
+  headers: {
+    "accept": "application/json",
+    "x-requested-with": "xmlhttprequest",
+    "Access-Control-Allow-Origin": "*",
+    "Authorization": `Bearer ${API_KEY}`
+  }
+}
+
+let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 document.querySelector(`.search`).addEventListener('click', e => {
   searchInput = document.querySelector(`.input`).value
-  console.log(searchInput)
   URL = `https://api.yelp.com/v3/businesses/search?location=${searchInput}&limit=25`;
   queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
-  fetch(queryURL, {
-    method: "GET",
-    headers: {
-      "accept": "application/json",
-      "x-requested-with": "xmlhttprequest",
-      "Access-Control-Allow-Origin": "*",
-      "Authorization": `Bearer ${API_KEY}`
-    }
-  })
+
+  fetch(queryURL, yelpObject)
     .then(r => r.json())
     .then(r => {
-      console.log(r)
       let i = Math.floor(Math.random() * 25)
-      console.log(i)
-      let newListing = document.createElement(`div`)
-      newListing.innerHTML = `
+      let currentBusinessId = r.businesses[i].id
+
+      // New fetch with current business
+      URL = `https://api.yelp.com/v3/businesses/${currentBusinessId}`
+      queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
+      fetch(queryURL, yelpObject)
+        .then(r => r.json())
+        .then(r => {
+          console.log(r)
+          let newListing = document.createElement(`div`)
+          newListing.innerHTML = `
       <div class="card">
         <div class="card-image">
           <figure class="image is-4by3">
-            <img class="business-img" src="${r.businesses[i].image_url}" alt="${r.businesses[i].name}">
+            <img class="business-img" src="${r.image_url}" alt="${r.name}">
           </figure>
         </div>
       <div class="card-content">
         <div class="media">
           <div class="media-content">
-            <p class="title is-4">${r.businesses[i].name}</p>
+            <p class="title is-4">${r.name}</p>
           </div>
         </div>
       <div class="content">
-        <p>Address: ${r.businesses[i].location.display_address[0]}, ${r.businesses[i].location.display_address[1]}</p>
-        <p>Phone: ${r.businesses[i].display_phone}</p>
-        <p>For business hours, reviews, and menus, <a href="${r.businesses[i].url}">click here</a>
+        <p>Address: ${r.location.display_address[0]}, ${r.location.display_address[1]}</p>
+        <p>Phone: ${r.display_phone}</p>
+        <p>For business hours, reviews, and menus, <a href="${r.url}">click here</a>
         </div>
       </div>
     </div>
     `
-      document.querySelector(`#results`).innerHTML = ``
-      document.querySelector(`#results`).append(newListing)
+          document.querySelector(`#results`).innerHTML = ``
+          document.querySelector(`#results`).append(newListing)
+          let businessHours = r.hours[0].open
+          // businessHours.forEach(item => {
+          //   console.log(item)
+          //   // Create elem, add innerHTML, append to results div
+          //   let hoursElem = document.createElement(`p`)
+          //   hoursElem.innerHTML = 
+          // })
+        })
     })
   document.querySelector(`.search2`).style.display = `block`
   document.querySelector(`.more`).style.display = `block`
-
 });
 //
 
