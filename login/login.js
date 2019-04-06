8c8dbe41609b6ed767d3f213ae883994b82e1502
Dante Perez <red.dante888@gmail.com>
2019-04-06 12:00:09 -0700

    morning changes and logo

--- login/login.js
+++ login/login.js
@@ -8,4 +8,24 @@
     messagingSenderId: "848170258525"
   };
   firebase.initializeApp(config);
+
+//togglenightmode
+  function toggleNightMode() {
+    var color = document.getElementById('mydiv').style.color;
+      
+    var backgroundColor = document.getElementById('mydiv').style.backgroundColor;
+    
+      
+    if (color == "black" && backgroundColor == "white") {
+          document.getElementById('mydiv').style.color="white";
+          document.getElementById('mydiv').style.backgroundColor="black";
+          document.getElementById('mydiv').style.fontFamily="Arial";
+          
+      } else {
+          document.getElementById('mydiv').style.color="black";
+          document.getElementById('mydiv').style.backgroundColor="white";
+          document.getElementById('mydiv').style.fontFamily="Times New Roman";
+      }
+  }
+  
  
