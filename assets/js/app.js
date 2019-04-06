fc04548dba555577da62279e0bebdbf737e195df
Mahya <safavis@uci.edu>
2019-04-06 10:23:57 -0700

    login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,21 @@
 let drinkWords =["SHOT O'CLOCK!", "It's 5 o'clock somwhere!", "Beer is proof that God loves us and wants us to be happy.",
 "DILLY DILLY!", "Alcohol may be man’s worst enemy, but the Bible says love your enemy.", "Everybody’s got to believe in something. I believe I’ll have another beer."]
 // On click fetchs and randomize returned data
+var config = {
+    apiKey: "AIzaSyALJmJ2VVKSDLb7wNk3HGxdXM1OH_KfE30",
+    authDomain: "indecisive-a9a51.firebaseapp.com",
+    databaseURL: "https://indecisive-a9a51.firebaseio.com",
+    projectId: "indecisive-a9a51",
+    storageBucket: "indecisive-a9a51.appspot.com",
+    messagingSenderId: "848170258525"
+  };
+  firebase.initializeApp(config);
+  let user=firebase.firestore()
+  let name
 document.addEventListener('click', e =>{
     e.preventDefault()
-    let getBeerCity = document.getElementById('cityVal').value
     if(e.target.id === 'randomData'){
+        let getBeerCity = document.getElementById('cityVal').value
         fetch('http://beermapping.com/webservice/loccity/b7e0022555c2b92e984c3bc704449aba/'+ getBeerCity + '&s=json')
             .then( r => r.json())
             .then( r =>{
@@ -39,5 +50,13 @@ document.addEventListener('click', e =>{
                     console.log(r)
                 })
             .catch(console.error)  
+    }
+    if(e.target.id==="signup"){
+        name=document.querySelector(".name").value
+        let email=document.querySelector(".email").value
+        let username=document.querySelector(".usernm").value
+        let password=document.querySelector(".pass").value
+        console.log({'name':name,'email':email,'username':username,'password':password})
+        user.collection('user_login').doc(name).set({'name':name,'email':email,'username':username,'password':password})
     }
 })
