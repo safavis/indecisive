7a7ab3d20916434858c99401fbf6d8f403770b65
dien <iamdiennn@gmail.com>
2019-04-07 21:02:33 -0700

    changes to brewry api functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 let drinkWords =["SHOT O'CLOCK!", "It's 5 o'clock somwhere!", "Beer is proof that God loves us and wants us to be happy.",
 "DILLY DILLY!", "Alcohol may be man’s worst enemy, but the Bible says love your enemy.", "Everybody’s got to believe in something. I believe I’ll have another beer."]
+let beerImg = ['./assets/image/brew01.jpg', './assets/image/brew02.jpg', './assets/image/brew03.jpg', './assets/image/brew04.jpg', './assets/image/brew05.jpg', './assets/image/brew06.jpg' ]
 // On click fetchs and randomize returned data
 var config = {
     apiKey: "AIzaSyALJmJ2VVKSDLb7wNk3HGxdXM1OH_KfE30",
@@ -12,44 +13,54 @@ var config = {
   firebase.initializeApp(config);
   let user=firebase.firestore()
   let name
-document.addEventListener('click', e =>{
+//   Beer Mapping API
+document.querySelector('.search').addEventListener('click', e =>{
    // e.preventDefault()
-    if(e.target.id === 'randomData'){
-        let getBeerCity = document.getElementById('cityVal').value
+   let beerCheck = document.querySelector('#beerCheck').checked
+    if(beerCheck === true){
+        let getBeerCity = document.querySelector('.input').value
         fetch('http://beermapping.com/webservice/loccity/b7e0022555c2b92e984c3bc704449aba/'+ getBeerCity + '&s=json')
             .then( r => r.json())
             .then( r =>{
                 let ranBrew = r[Math.floor(Math.random()* r.length)]
                 let ranWords = drinkWords[Math.floor(Math.random()*drinkWords.length)]
-                document.querySelector('#beerData').innerHTML = `
+                let ranImg = beerImg[Math.floor(Math.random()*beerImg.length)]
+                let beerListing = document.createElement('div')
+                beerListing.innerHTML = `
+                <h1 class="title is-1">Pub Choice!</h1>
                 <div class="card" id="beerCard">
-                <div class="card-image" id="beerImage">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img class="business-img" src="${ranImg}" alt="${ranBrew.name}">
+                  </figure>
                 </div>
-                    <div class="card-content">
-                        <div class="media">
-                        <div class="media-content">
-                            <p><a class="title is-4" src="${ranBrew.proxylink}">${ranBrew.name}</a></p>
-                            <p class="subtitle is-6">${ranBrew.phone}</p>
-                            <p class="subtitle is-6">${ranBrew.street}. ${ranBrew.city}, ${ranBrew.state} ${ranBrew.zip}</p>
-                        </div>
-                        </div>
-                        <div class="content">
-                        "${ranWords}"
-                        </div>
-                    </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-content">
+                    <p class="title is-4">${ranBrew.name}</p>
+                  </div>
                 </div>
+              <div class="content">
+                <p>Address: ${ranBrew.street}. ${ranBrew.city}, ${ranBrew.state} ${ranBrew.zip}</p>
+                <p>Phone: ${ranBrew.phone}</p>
+                <p>For business hours, reviews, and menus, <a href="${ranBrew.reviewlink}">click here</a>
+                </div>
+                <div class="content"><Strong>"${ranWords}"</strong></div>
+              </div>
+            </div>
                 `
+                document.querySelector(`#beerResults`).innerHTML = ``
+                document.querySelector(`#beerResults`).append(beerListing)
+                document.querySelector('.newBeer').style.display = 'block'
+                document.querySelector('.moreBeer').style.display = 'block'
                 console.log(ranWords)
                 console.log(ranBrew)
-                console.log(r)
-                // Using result of 1st request to use in 2nd request
-                return fetch('http://beermapping.com/webservice/locimage/b7e0022555c2b92e984c3bc704449aba/'+ ranBrew.id + '&s=json')
-            }).then(data => data.json())
-                .then(r =>{
-                   
-                    console.log(r)
-                })
+            })
             .catch(console.error)  
+    } else{
+        document.querySelector(`#beerResults`).innerHTML = ``
+        document.querySelector('.newBeer').style.display = 'none'
+        document.querySelector('.moreBeer').style.display = 'none'
     }
     if(e.target.id==="signup"){
         name=document.querySelector(".name").value
@@ -60,6 +71,43 @@ document.addEventListener('click', e =>{
         user.collection('user_login').doc(name).set({'name':name,'email':email,'username':username,'password':password})
     }
 })
+document.querySelector('.moreBeer').addEventListener('click', e =>{
+    document.querySelector(`#beerResults`).innerHTML = ``
+         let getBeerCity = document.querySelector('.input').value
+         fetch('http://beermapping.com/webservice/loccity/b7e0022555c2b92e984c3bc704449aba/'+ getBeerCity + '&s=json')
+            .then(r => r.json())
+            .then(r => {
+                console.log(r)
+                for(let i = 0; i <r.length;i++){ 
+                    let beerListing = document.createElement('div')
+                    beerListing.innerHTML = `
+                    
+                    <div class="card" id="beerCard">
+                    <div class="card-image">
+                      <figure class="image is-4by3">
+                        <img class="business-img" src="${beerImg[i]}" alt="${r[i].name}">
+                      </figure>
+                    </div>
+                  <div class="card-content">
+                    <div class="media">
+                      <div class="media-content">
+                        <p class="title is-4">${r[i].name}</p>
+                      </div>
+                    </div>
+                  <div class="content">
+                    <p>Address: ${r[i].street}. ${r[i].city}, ${r[i].state} ${r[i].zip}</p>
+                    <p>Phone: ${r[i].phone}</p>
+                    <p>For business hours, reviews, and menus, <a href="${r[i].reviewlink}">click here</a>
+                    </div>
+                    <div class="content"><Strong>""</strong></div>
+                  </div>
+                </div>`
+                document.querySelector('#beerResults').append(beerListing)
+                document.querySelector('.newBeer').style.display = 'block'
+                document.querySelector('.moreBeer').style.display = 'block'
+            }
+            })
+        })
 // Yelp API code
 // let URL = 'https://api.yelp.com/v3/businesses/search?location=40515&term&categories=vet&limit=10';
 // let URL = `https://api.yelp.com/v3/businesses/search?location=${searchInput}&limit=10`;
@@ -109,6 +157,7 @@ document.querySelector(`.search`).addEventListener('click', e => {
       </div>
     </div>
     `
+
       document.querySelector(`#results`).innerHTML = ``
       document.querySelector(`#results`).append(newListing)
     })
