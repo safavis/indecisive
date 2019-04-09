346a3117af0a07ea254a22f53473198d42912361
C.Nguyen <cdkedits@gmail.com>
2019-04-09 15:21:52 -0700

    added favicon, adjusted javascript on search buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,27 @@
-let drinkWords =["SHOT O'CLOCK!", "It's 5 o'clock somwhere!", "Beer is proof that God loves us and wants us to be happy.",
-"DILLY DILLY!", "Alcohol may be man’s worst enemy, but the Bible says love your enemy.", "Everybody’s got to believe in something. I believe I’ll have another beer."]
+let drinkWords = ["SHOT O'CLOCK!", "It's 5 o'clock somwhere!", "Beer is proof that God loves us and wants us to be happy.",
+  "DILLY DILLY!", "Alcohol may be man’s worst enemy, but the Bible says love your enemy.", "Everybody’s got to believe in something. I believe I’ll have another beer."]
 // On click fetchs and randomize returned data
 var config = {
-    apiKey: "AIzaSyALJmJ2VVKSDLb7wNk3HGxdXM1OH_KfE30",
-    authDomain: "indecisive-a9a51.firebaseapp.com",
-    databaseURL: "https://indecisive-a9a51.firebaseio.com",
-    projectId: "indecisive-a9a51",
-    storageBucket: "indecisive-a9a51.appspot.com",
-    messagingSenderId: "848170258525"
-  };
-  firebase.initializeApp(config);
-  let user=firebase.firestore()
-  let name
-document.addEventListener('click', e =>{
-    e.preventDefault()
-    if(e.target.id === 'randomData'){
-        let getBeerCity = document.getElementById('cityVal').value
-        fetch('http://beermapping.com/webservice/loccity/b7e0022555c2b92e984c3bc704449aba/'+ getBeerCity + '&s=json')
-            .then( r => r.json())
-            .then( r =>{
-                let ranBrew = r[Math.floor(Math.random()* r.length)]
-                let ranWords = drinkWords[Math.floor(Math.random()*drinkWords.length)]
-                document.querySelector('#beerData').innerHTML = `
+  apiKey: "AIzaSyALJmJ2VVKSDLb7wNk3HGxdXM1OH_KfE30",
+  authDomain: "indecisive-a9a51.firebaseapp.com",
+  databaseURL: "https://indecisive-a9a51.firebaseio.com",
+  projectId: "indecisive-a9a51",
+  storageBucket: "indecisive-a9a51.appspot.com",
+  messagingSenderId: "848170258525"
+};
+firebase.initializeApp(config);
+let user = firebase.firestore()
+let name
+document.addEventListener('click', e => {
+  // e.preventDefault()
+  if (e.target.id === 'randomData') {
+    let getBeerCity = document.getElementById('cityVal').value
+    fetch('http://beermapping.com/webservice/loccity/b7e0022555c2b92e984c3bc704449aba/' + getBeerCity + '&s=json')
+      .then(r => r.json())
+      .then(r => {
+        let ranBrew = r[Math.floor(Math.random() * r.length)]
+        let ranWords = drinkWords[Math.floor(Math.random() * drinkWords.length)]
+        document.querySelector('#beerData').innerHTML = `
                 <div class="card" id="beerCard">
                 <div class="card-image" id="beerImage">
                 </div>
@@ -39,27 +39,28 @@ document.addEventListener('click', e =>{
                     </div>
                 </div>
                 `
-                console.log(ranWords)
-                console.log(ranBrew)
-                console.log(r)
-                // Using result of 1st request to use in 2nd request
-                return fetch('http://beermapping.com/webservice/locimage/b7e0022555c2b92e984c3bc704449aba/'+ ranBrew.id + '&s=json')
-            }).then(data => data.json())
-                .then(r =>{
-                   
-                    console.log(r)
-                })
-            .catch(console.error)  
-    }
-    if(e.target.id==="signup"){
-        name=document.querySelector(".name").value
-        let email=document.querySelector(".email").value
-        let username=document.querySelector(".usernm").value
-        let password=document.querySelector(".pass").value
-        console.log({'name':name,'email':email,'username':username,'password':password})
-        user.collection('user_login').doc(name).set({'name':name,'email':email,'username':username,'password':password})
-    }
+        console.log(ranWords)
+        console.log(ranBrew)
+        console.log(r)
+        // Using result of 1st request to use in 2nd request
+        return fetch('http://beermapping.com/webservice/locimage/b7e0022555c2b92e984c3bc704449aba/' + ranBrew.id + '&s=json')
+      }).then(data => data.json())
+      .then(r => {
+
+        console.log(r)
+      })
+      .catch(console.error)
+  }
+  if (e.target.id === "signup") {
+    name = document.querySelector(".name").value
+    let email = document.querySelector(".email").value
+    let username = document.querySelector(".usernm").value
+    let password = document.querySelector(".pass").value
+    console.log({ 'name': name, 'email': email, 'username': username, 'password': password })
+    user.collection('user_login').doc(name).set({ 'name': name, 'email': email, 'username': username, 'password': password })
+  }
 })
+
 // Yelp API code
 let URL
 
@@ -77,7 +78,15 @@ let yelpObject = {
   }
 }
 
-let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let weekdays = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
+
+const ratingFunc = () => {
+  const starImg = document.createElement(`img`)
+  starImg.setAttribute(`src`, `./assets/image/rating-star-icon.jpg`)
+  starImg.setAttribute(`alt`, `rating star icon`)
+  starImg.className = `stars`
+  document.querySelector(`.ratingsDiv`).append(starImg)
+}
 
 document.querySelector(`.search`).addEventListener('click', e => {
   searchInput = document.querySelector(`.input`).value
@@ -98,7 +107,8 @@ document.querySelector(`.search`).addEventListener('click', e => {
         .then(r => {
           console.log(r)
           let newListing = document.createElement(`div`)
-          newListing.innerHTML = `
+          if (r.hours[0].open.length === 7) {
+            newListing.innerHTML = `
       <div class="card">
         <div class="card-image">
           <figure class="image is-4by3">
@@ -109,29 +119,91 @@ document.querySelector(`.search`).addEventListener('click', e => {
         <div class="media">
           <div class="media-content">
             <p class="title is-4">${r.name}</p>
+            <div class ="ratingsDiv content">
+              <h5>Rating out of 5:</h5>
+            </div>
           </div>
         </div>
-      <div class="content">
-        <p>Address: ${r.location.display_address[0]}, ${r.location.display_address[1]}</p>
-        <p>Phone: ${r.display_phone}</p>
-        <p>For business hours, reviews, and menus, <a href="${r.url}">click here</a>
+      <div class="content columns">
+        <div class="location column is-half">
+        <h5>Address:</h5>
+        <p class="address"></p>
+        <h5>Phone number:</h5>
+        <p>${r.display_phone}</p>
+        <h5>For customer reviews and menus, <a href="${r.url}">click here</a></h5>
+        </div>
+        <div class="hours column is-half">
+          <div>
+            <h5>Business hours:</h5>
+          </div>
+        </div>
         </div>
       </div>
     </div>
     `
-          document.querySelector(`#results`).innerHTML = ``
-          document.querySelector(`#results`).append(newListing)
-          let businessHours = r.hours[0].open
-          businessHours.forEach(item => {
-            // console.log(item)
-            let start = item.start 
-            console.log(start)
-            moment(start, hhmm)
-            // let end = 
-            // Create elem, add innerHTML, append to results div
-            // let hoursElem = document.createElement(`p`)
-            // hoursElem.innerHTML = 
-          })
+            document.querySelector(`#results`).innerHTML = ``
+            document.querySelector(`#results`).append(newListing)
+            for (let i = 0; i < r.location.display_address.length; i++) {
+              let addressPart = document.createElement(`p`)
+              addressPart.innerHTML = `${r.location.display_address[i]} `
+              document.querySelector(`.address`).append(addressPart)
+            }
+            for (let i = 0; i < r.rating; i++) {
+              ratingFunc()
+            }
+            let businessHours = r.hours[0].open
+            console.log(r)
+            businessHours.forEach((item, i) => {
+              let openingHours = moment(item.start, `HHmm`).format(`hh:mm a`)
+              let closingHours = moment(item.end, `HHmm`).format(`hh:mm a`)
+              // Create elem, add innerHTML, append to results div
+              let hoursElem = document.createElement(`p`)
+              hoursElem.innerHTML = `
+            <p>${weekdays[i]}: ${openingHours} - ${closingHours}</p>
+            `
+              document.querySelector(`.hours`).append(hoursElem)
+
+            })
+          } else {
+            newListing.innerHTML = `
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img class="business-img" src="${r.image_url}" alt="${r.name}">
+          </figure>
+        </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">${r.name}</p>
+            <div class ="ratingsDiv content">
+              <h5>Rating out of 5:</h5>
+            </div>
+          </div>
+        </div>
+      <div class="content columns">
+        <div class="location column is-half">
+        <h5>Address:</h5>
+        <p class="address"></p>
+        <h5>Phone number:</h5>
+        <p>${r.display_phone}</p>
+        <h5>For business hours, customer reviews, and menus, <a href="${r.url}">click here</a></h5>
+        </div>
+        </div>
+      </div>
+    </div>
+    `
+            document.querySelector(`#results`).innerHTML = ``
+            document.querySelector(`#results`).append(newListing)
+            for (let i = 0; i < r.location.display_address.length; i++) {
+              let addressPart = document.createElement(`p`)
+              addressPart.innerHTML = `${r.location.display_address[i]} `
+              document.querySelector(`.address`).append(addressPart)
+            }
+            for (let i = 0; i < r.rating; i++) {
+              ratingFunc()
+            }
+          }
         })
     })
   document.querySelector(`.search2`).style.display = `block`
@@ -140,48 +212,124 @@ document.querySelector(`.search`).addEventListener('click', e => {
 //
 
 document.querySelector(`.search2`).addEventListener('click', e => {
-  document.querySelector(`#results`).innerHTML = ``
   searchInput = document.querySelector(`.input`).value
-  console.log(searchInput)
   URL = `https://api.yelp.com/v3/businesses/search?location=${searchInput}&limit=25`;
   queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
-  fetch(queryURL, {
-    method: "GET",
-    headers: {
-      "accept": "application/json",
-      "x-requested-with": "xmlhttprequest",
-      "Access-Control-Allow-Origin": "*",
-      "Authorization": `Bearer ${API_KEY}`
-    }
-  })
+
+  fetch(queryURL, yelpObject)
     .then(r => r.json())
     .then(r => {
-      console.log(r)
       let i = Math.floor(Math.random() * 25)
-      let newListing = document.createElement(`div`)
-      newListing.innerHTML = `
+      let currentBusinessId = r.businesses[i].id
+
+      // New fetch with current business
+      URL = `https://api.yelp.com/v3/businesses/${currentBusinessId}`
+      queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
+      fetch(queryURL, yelpObject)
+        .then(r => r.json())
+        .then(r => {
+          console.log(r)
+          let newListing = document.createElement(`div`)
+          if (r.hours[0].open.length === 7) {
+            newListing.innerHTML = `
       <div class="card">
         <div class="card-image">
           <figure class="image is-4by3">
-            <img class="business-img" src="${r.businesses[i].image_url}" alt="${r.businesses[i].name}">
+            <img class="business-img" src="${r.image_url}" alt="${r.name}">
           </figure>
         </div>
       <div class="card-content">
         <div class="media">
           <div class="media-content">
-            <p class="title is-4">${r.businesses[i].name}</p>
+            <p class="title is-4">${r.name}</p>
+            <div class ="ratingsDiv content">
+              <h5>Rating out of 5:</h5>
+            </div>
           </div>
         </div>
-      <div class="content">
-        <p>Address: ${r.businesses[i].location.display_address[0]}, ${r.businesses[i].location.display_address[1]}</p>
-        <p>Phone: ${r.businesses[i].display_phone}</p>
-        <p>For business hours, reviews, and menus, <a href="${r.businesses[i].url}">click here</a>
+      <div class="content columns">
+        <div class="location column is-half">
+        <h5>Address:</h5>
+        <p class="address"></p>
+        <h5>Phone number:</h5>
+        <p>${r.display_phone}</p>
+        <h5>For customer reviews and menus, <a href="${r.url}">click here</a></h5>
+        </div>
+        <div class="hours column is-half">
+          <div>
+            <h5>Business hours:</h5>
+          </div>
+        </div>
         </div>
       </div>
     </div>
     `
-      document.querySelector(`#results`).append(newListing)
+            document.querySelector(`#results`).innerHTML = ``
+            document.querySelector(`#results`).append(newListing)
+            for (let i = 0; i < r.location.display_address.length; i++) {
+              let addressPart = document.createElement(`p`)
+              addressPart.innerHTML = `${r.location.display_address[i]} `
+              document.querySelector(`.address`).append(addressPart)
+            }
+            for (let i = 0; i < r.rating; i++) {
+              ratingFunc()
+            }
+            let businessHours = r.hours[0].open
+            console.log(r)
+            businessHours.forEach((item, i) => {
+              let openingHours = moment(item.start, `HHmm`).format(`hh:mm a`)
+              let closingHours = moment(item.end, `HHmm`).format(`hh:mm a`)
+              let hoursElem = document.createElement(`p`)
+              hoursElem.innerHTML = `
+            <p>${weekdays[i]}: ${openingHours} - ${closingHours}</p>
+            `
+              document.querySelector(`.hours`).append(hoursElem)
+
+            })
+          } else {
+            newListing.innerHTML = `
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img class="business-img" src="${r.image_url}" alt="${r.name}">
+          </figure>
+        </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">${r.name}</p>
+            <div class ="ratingsDiv content">
+              <h5>Rating out of 5:</h5>
+            </div>
+          </div>
+        </div>
+      <div class="content columns">
+        <div class="location column is-half">
+        <h5>Address:</h5>
+        <p class="address"></p>
+        <h5>Phone number:</h5>
+        <p>${r.display_phone}</p>
+        <h5>For business hours, customer reviews, and menus, <a href="${r.url}">click here</a></h5>
+        </div>
+        </div>
+      </div>
+    </div>
+    `
+            document.querySelector(`#results`).innerHTML = ``
+            document.querySelector(`#results`).append(newListing)
+            for (let i = 0; i < r.location.display_address.length; i++) {
+              let addressPart = document.createElement(`p`)
+              addressPart.innerHTML = `${r.location.display_address[i]} `
+              document.querySelector(`.address`).append(addressPart)
+            }
+            for (let i = 0; i < r.rating; i++) {
+              ratingFunc()
+            }
+          }
+        })
     })
+  document.querySelector(`.search2`).style.display = `block`
+  document.querySelector(`.more`).style.display = `block`
 });
 
 document.querySelector(`.more`).addEventListener('click', e => {
@@ -190,15 +338,7 @@ document.querySelector(`.more`).addEventListener('click', e => {
   console.log(searchInput)
   URL = `https://api.yelp.com/v3/businesses/search?location=${searchInput}&limit=10`;
   queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
-  fetch(queryURL, {
-    method: "GET",
-    headers: {
-      "accept": "application/json",
-      "x-requested-with": "xmlhttprequest",
-      "Access-Control-Allow-Origin": "*",
-      "Authorization": `Bearer ${API_KEY}`
-    }
-  })
+  fetch(queryURL, yelpObject)
     .then(r => r.json())
     .then(r => {
       console.log(r)
@@ -218,9 +358,11 @@ document.querySelector(`.more`).addEventListener('click', e => {
           </div>
         </div>
       <div class="content">
-        <p>Address: ${r.businesses[i].location.display_address[0]}, ${r.businesses[i].location.display_address[1]}</p>
-        <p>Phone: ${r.businesses[i].display_phone}</p>
-        <p>For business hours, reviews, and menus, <a href="${r.businesses[i].url}">click here</a>
+        <h5>Address:</h5>
+        <p>${r.businesses[i].location.display_address[0]}, ${r.businesses[i].location.display_address[1]}</p>
+        <h5>Phone:</h5>
+        <p>${r.businesses[i].display_phone}</p>
+        <h5>For business hours, reviews, and menus, <a href="${r.businesses[i].url}">click here</a></h5>
         </div>
       </div>
     </div>
