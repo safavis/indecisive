5a19db7c354df0cd672a8645365a2e7c997b59a7
Dante Perez <red.dante888@gmail.com>
2019-04-09 19:13:43 -0700

    nightmodejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ var config = {
   let user=firebase.firestore()
   let name
 document.addEventListener('click', e =>{
-    e.preventDefault()
+    // e.preventDefault()
     if(e.target.id === 'randomData'){
         let getBeerCity = document.getElementById('cityVal').value
         fetch('http://beermapping.com/webservice/loccity/b7e0022555c2b92e984c3bc704449aba/'+ getBeerCity + '&s=json')
@@ -68,54 +68,54 @@ const API_KEY = 'M7IgpDGg-9kXeDXpl5Yj9A9_33reRjvfXANnx3RvJzphIo_pAXUEvP5FubmdzBV
 
 let queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
 
-document.querySelector(`.search`).addEventListener('click', e => {
-  searchInput = document.querySelector(`.input`).value
-  console.log(searchInput)
-  URL = `https://api.yelp.com/v3/businesses/search?location=${searchInput}&limit=25`;
-  queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
-  fetch(queryURL, {
-    method: "GET",
-    headers: {
-      "accept": "application/json",
-      "x-requested-with": "xmlhttprequest",
-      "Access-Control-Allow-Origin": "*",
-      "Authorization": `Bearer ${API_KEY}`
-    }
-  })
-    .then(r => r.json())
-    .then(r => {
-      console.log(r)
-      let i = Math.floor(Math.random() * 25)
-      console.log(i)
-      let newListing = document.createElement(`div`)
-      newListing.innerHTML = `
-      <div class="card">
-        <div class="card-image">
-          <figure class="image is-4by3">
-            <img class="business-img" src="${r.businesses[i].image_url}" alt="${r.businesses[i].name}">
-          </figure>
-        </div>
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            <p class="title is-4">${r.businesses[i].name}</p>
-          </div>
-        </div>
-      <div class="content">
-        <p>Address: ${r.businesses[i].location.display_address[0]}, ${r.businesses[i].location.display_address[1]}</p>
-        <p>Phone: ${r.businesses[i].display_phone}</p>
-        <p>For business hours, reviews, and menus, <a href="${r.businesses[i].url}">click here</a>
-        </div>
-      </div>
-    </div>
-    `
-      document.querySelector(`#results`).innerHTML = ``
-      document.querySelector(`#results`).append(newListing)
-    })
-  document.querySelector(`.search2`).style.display = `block`
-  document.querySelector(`.more`).style.display = `block`
+// document.querySelector(`.search`).addEventListener('click', e => {
+  // searchInput = document.querySelector(`.input`).value``
+//   console.log(searchInput)
+//   URL = `https://api.yelp.com/v3/businesses/search?location=${searchInput}&limit=25`;
+//   queryURL = `https://cors-anywhere.herokuapp.com/${URL}`;
+//   fetch(queryURL, {
+//     method: "GET",
+//     headers: {
+//       "accept": "application/json",
+//       "x-requested-with": "xmlhttprequest",
+//       "Access-Control-Allow-Origin": "*",
+//       "Authorization": `Bearer ${API_KEY}`
+//     }
+//   })
+//     .then(r => r.json())
+//     .then(r => {
+//       console.log(r)
+//       let i = Math.floor(Math.random() * 25)
+//       console.log(i)
+//       let newListing = document.createElement(`div`)
+//       newListing.innerHTML = `
+//       <div class="card">
+//         <div class="card-image">
+//           <figure class="image is-4by3">
+//             <img class="business-img" src="${r.businesses[i].image_url}" alt="${r.businesses[i].name}">
+//           </figure>
+//         </div>
+//       <div class="card-content">
+//         <div class="media">
+//           <div class="media-content">
+//             <p class="title is-4">${r.businesses[i].name}</p>
+//           </div>
+//         </div>
+//       <div class="content">
+//         <p>Address: ${r.businesses[i].location.display_address[0]}, ${r.businesses[i].location.display_address[1]}</p>
+//         <p>Phone: ${r.businesses[i].display_phone}</p>
+//         <p>For business hours, reviews, and menus, <a href="${r.businesses[i].url}">click here</a>
+//         </div>
+//       </div>
+//     </div>
+//     `
+//       document.querySelector(`#results`).innerHTML = ``
+//       document.querySelector(`#results`).append(newListing)
+//     })
+//   document.querySelector(`.search2`).style.display = `block`
+//   document.querySelector(`.more`).style.display = `block`
 
-});
+// });
 //
 
 document.querySelector(`.search2`).addEventListener('click', e => {
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,31 +1,25 @@
- // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyALJmJ2VVKSDLb7wNk3HGxdXM1OH_KfE30",
-    authDomain: "indecisive-a9a51.firebaseapp.com",
-    databaseURL: "https://indecisive-a9a51.firebaseio.com",
-    projectId: "indecisive-a9a51",
-    storageBucket: "indecisive-a9a51.appspot.com",
-    messagingSenderId: "848170258525"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyALJmJ2VVKSDLb7wNk3HGxdXM1OH_KfE30",
+  authDomain: "indecisive-a9a51.firebaseapp.com",
+  databaseURL: "https://indecisive-a9a51.firebaseio.com",
+  projectId: "indecisive-a9a51",
+  storageBucket: "indecisive-a9a51.appspot.com",
+  messagingSenderId: "848170258525"
+};
+firebase.initializeApp(config);
 
 //togglenightmode
-  function toggleNightMode() {
-    var color = document.getElementById('mydiv').style.color;
-      
-    var backgroundColor = document.getElementById('mydiv').style.backgroundColor;
-    
-      
-    if (color == "black" && backgroundColor == "white") {
-          document.getElementById('mydiv').style.color="white";
-          document.getElementById('mydiv').style.backgroundColor="black";
-          document.getElementById('mydiv').style.fontFamily="Arial";
-          
-      } else {
-          document.getElementById('mydiv').style.color="black";
-          document.getElementById('mydiv').style.backgroundColor="white";
-          document.getElementById('mydiv').style.fontFamily="Times New Roman";
-      }
-  }
-  
- 
+const normal = "./assets/css/login.css"
+const nightmode = "./assets/css/loginNight.css"
+let nightmodeOn = false
+let currentmode;
+const toggleNightMode = function () {
+  nightmodeOn ? currentmode = nightmode : currentmode = normal
+  var newstyle = document.createElement("link"); 
+  newstyle.setAttribute("rel", "stylesheet");
+  newstyle.setAttribute("type", "text/css");
+  newstyle.setAttribute("href", currentmode); 
+  document.getElementsByTagName("head")[0].appendChild(newstyle);
+  nightmodeOn = !nightmodeOn
+}
